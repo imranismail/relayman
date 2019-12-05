dcdb0eb4722f76fd5ab2d8a52c1c1017b95d582e
Imran Ismail <imran.codely@gmail.com>
2019-12-06 01:42:04 +0800

    Update url

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :relayman, RelaymanWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  url: [host: "relayman.app.fave.ninja", port: 443],
   check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
