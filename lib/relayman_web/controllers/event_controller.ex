f9da116895c364153820bc95dd7700d3bfa90977
Imran Ismail <imran.codely@gmail.com>
2019-12-06 11:52:06 +0800

    Bump parameters and fix formatter

--- lib/relayman_web/controllers/event_controller.ex
+++ lib/relayman_web/controllers/event_controller.ex
@@ -3,15 +3,15 @@ defmodule RelaymanWeb.EventController do
   use Parameters
 
   params do
-    requires(:specversion, :string, default: "1.0")
-    requires(:id, :string)
-    requires(:source, :string)
-    requires(:type, :string)
-    optional(:datacontenttype, :string)
-    optional(:dataschema, :string)
-    optional(:subject, :string)
-    optional(:time, :string)
-    optional(:data, :map)
+    requires :specversion, :string, default: "1.0"
+    requires :id, :string
+    requires :source, :string
+    requires :type, :string
+    optional :datacontenttype, :string
+    optional :dataschema, :string
+    optional :subject, :string
+    optional :time, :string
+    optional :data, :map
   end
 
   def create(conn, params) do
