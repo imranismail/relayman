202bc6ed52a9dd26884f2f21ed6330a7c906205f
Imran Ismail <imran.codely@gmail.com>
2020-07-23 20:32:40 +0800

    Setup redis connection

---
+++ lib/redis.ex
@@ -0,0 +1,42 @@
+defmodule Redis do
+  def child_spec(opts) do
+    merged_opts = opts(name: __MODULE__)
+    merged_opts = Keyword.merge(merged_opts, opts)
+    Redix.child_spec(merged_opts)
+  end
+
+  def opts(base_opts \\ []) do
+    host = System.get_env("RELAYMAN_REDIS_HOST", "127.0.0.1")
+
+    password = System.get_env("RELAYMAN_REDIS_PASSWORD")
+
+    port =
+      "RELAYMAN_REDIS_PORT"
+      |> System.get_env("6379")
+      |> String.to_integer()
+
+    ssl_enabled? = System.get_env("RELAYMAN_REDIS_SSL_ENABLED") == "true"
+
+    base_opts
+    |> Keyword.put(:host, host)
+    |> Keyword.put(:password, password)
+    |> Keyword.put(:ssl, ssl_enabled?)
+    |> Keyword.put(:port, port)
+    |> Keyword.put(:socket_opts, socket_opts())
+  end
+
+  defp socket_opts(base_opts \\ []) do
+    case System.fetch_env("RELAYMAN_REDIS_SOCKET_OPTSET") do
+      :error ->
+        base_opts
+
+      "elasticache" ->
+        Keyword.put(base_opts, :socket_opts,
+          customize_hostname_check: [
+            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
+          ]
+        )
+    end
+  end
+
+end
